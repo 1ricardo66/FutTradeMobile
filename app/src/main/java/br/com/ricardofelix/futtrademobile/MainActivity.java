092e9746b3ca835095d57e7ccf00ed15420e8ccf
Paulo Ricardo <ricardofps66@gmail.com>
2020-03-25 16:10:24 -0300

    Configurando onClick no FAB

--- app/src/main/java/br/com/ricardofelix/futtrademobile/MainActivity.java
+++ app/src/main/java/br/com/ricardofelix/futtrademobile/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.ricardofelix.futtrademobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.okBtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I am Working :P", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
